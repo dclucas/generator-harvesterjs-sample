fdf3f0f129c99902d82b02414c58175ef0228bc1
Diogo Lucas <diogo.lucas@gmail.com>
2015-03-09 03:00:52 +0000

    New structure, from new generator + cleanup script.

---
+++ .cleanup/cleanup.sh
@@ -0,0 +1,9 @@
+echo "Make sure you are running this from the project root..."
+rm -rf app
+rm -rf node_modules
+rm -rf test
+rm .editorconfig
+rm .gitignore
+rm .jshintrc
+rm *.log
+rm package.json
